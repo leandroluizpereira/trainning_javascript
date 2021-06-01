62db07c6e0c090f26949d61d23b5211145d2da22
leandroluizpereira <leandro.2306@hotmail.com>
2021-06-01 00:04:13 -0300

    style

--- createElement/funcoes.js
+++ createElement/funcoes.js
@@ -9,11 +9,62 @@ window.addEventListener("load",function(){
 
 })
 */
+function girar(){
+
+
+    var circulo = document.getElementById("circulo");
+ 
+    var tempo=Math.floor(Math.random()*10)+1;
+
+
+    switch(tempo){
+
+        case 0:
+            alert("parabéns");
+            break;
+
+        case 1:
+            alert("Girou com força");
+            circulo.style.animation="girar 2s linear";
+     
+            break;
+        case 2 :
+            circulo.style.animation="girar2 4s linear";
+            break;
+        case 3:
+            circulo.style.animation="girar3 5s linear";
+            break;
+        case 4:
+            circulo.style.animation="girar4 5s linear";
+            break;
+        case 5:
+            circulo.style.animation="girar2 6s linear";
+            break;
+        case 6:
+            circulo.style.animation="girar3 8s linear";
+            break;
+        case 8:
+            circulo.style.animation="girar4 4s linear";
+            break;
+        case 9:
+            circulo.style.animation="girar2 4s linear";
+            break;
+        case 10:
+            circulo.style.animation="girar4 4s linear";
+        default:
+            break;
+    }
+}
+   /* window.open('index.html','_self');*/
+
+
+
+
 var i=0;
  
 function inicia(){
-    
-    /*
+
+    /* Adicionar imagem 
 document.getElementById("btnAdd").addEventListener("click",function(){
 for(i=0 ;i<4;i++){
     var imagem = document.createElement("img");
@@ -27,13 +78,40 @@ document.body.appendChild(imagem);
 
 });
 */
+
+
+
+
+
 document.getElementById("liAdd").addEventListener("click",function(){
     var lista = document.createElement("li");
     lista.style.textAlign="center";
     var li = document.getElementById("input").value;
     lista.innerHTML=li;
-    document.body.appendChild(lista);   
+
+    var random =Math.floor(Math.random() * 10) + 1; 
+    switch(random){
+        case  1: 
+            lista.style.color="red";
+            break;
+        case 2 :
+            lista.style.color="blue";
+            break;
+        case 3:
+            lista.style.color="gray";
+            break;
+            case 4:
+                lista.style.color="green";
+                break;
+        default:
+           break;
+    }
+
+    document.body.appendChild(lista);
+
+
 });
 }
 window.addEventListener("load",inicia);
+
 
