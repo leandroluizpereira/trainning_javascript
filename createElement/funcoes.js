340ce5726fc2809e0a0e2c394e5cad6719507ebd
leandroluizpereira <leandro.2306@hotmail.com>
2021-05-31 22:20:51 -0300

    update

--- createElement/funcoes.js
+++ createElement/funcoes.js
@@ -12,6 +12,8 @@ window.addEventListener("load",function(){
 var i=0;
  
 function inicia(){
+    
+    /*
 document.getElementById("btnAdd").addEventListener("click",function(){
 for(i=0 ;i<4;i++){
     var imagem = document.createElement("img");
@@ -21,26 +23,17 @@ imagem.style.height="auto";
 imagem.style.float="left";
 imagem.style.margin="10px";
 document.body.appendChild(imagem);
-paragrafo.style.textAlign="center";
-
-
-
 }
 
-
-
 });
+*/
 document.getElementById("liAdd").addEventListener("click",function(){
     var lista = document.createElement("li");
-
     lista.style.textAlign="center";
     var li = document.getElementById("input").value;
-
     lista.innerHTML=li;
-    document.body.appendChild(lista);
+    document.body.appendChild(lista);   
 });
 }
-
-
 window.addEventListener("load",inicia);
 
